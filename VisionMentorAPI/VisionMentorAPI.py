4373072a1d966520dadd3dd945edaf1e06646e01
Dzolver <kaitoano0@gmail.com>
2017-08-12 19:53:19 +0530

    Increased Number of Fields for realistic testing

--- VisionMentorAPI/VisionMentorAPI.py
+++ VisionMentorAPI/VisionMentorAPI.py
@@ -1,13 +1,12 @@
 from flask import *
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
-import pprint
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://kaitoano0:boss359slasian!@maincluster-shard-00-00-resyk.mongodb.net:27017,maincluster-shard-00-01-resyk.mongodb.net:27017,maincluster-shard-00-02-resyk.mongodb.net:27017/testDB?ssl=true&replicaSet=mainCluster-shard-0&authSource=admin')
-db = client.civilian
-collection = db.city
+client = MongoClient('mongodb://kaitoano0:boss359slasian!@maincluster-shard-00-00-resyk.mongodb.net:27017,maincluster-shard-00-01-resyk.mongodb.net:27017,maincluster-shard-00-02-resyk.mongodb.net:27017/visionMentor?ssl=true&replicaSet=mainCluster-shard-0&authSource=admin')
+db = client.visionMentor
+collection = db.user
 
 #returns count of all the records in the database
 @app.route('/',methods=['GET'])
@@ -16,17 +15,17 @@ def getUserCount():
     return str(s)
 
 #returns the details of the records with the matching username
-@app.route('/user/<string:name>')
-def getUserNames(name):
-   for coll in collection.find({"Name":name},{"_id":0}):
-       print str(coll['Name'])
-   return str(coll['Name'])
+@app.route('/user/<string:Username>')
+def getUserNames(Username):
+   for coll in collection.find({"Name":Username},{"_id":0}):
+       print str(coll['Username'])
+   return str(coll['Username'])
 
 #registers a new user and updates the total count of users
-@app.route('/register/<string:name>')
-def registerUser(name):
-    if collection.count({"Name":name}) == 0:
-        new_user = {"Name": name}
+@app.route('/register/<string:Username>')
+def registerUser(Username):
+    if collection.count({"Username":Username}) == 0:
+        new_user = {"Username": Username}
         collection.insert_one(new_user).inserted_id
         print ("Successfully registered")
         return str(collection.count())
@@ -41,7 +40,7 @@ def showAll():
     i=0
     sentence = ""
     for coll in collection.find():
-        output.append(str(coll['Name']))
+        output.append(str(coll['Username']))
         i=i+1
     for j in range(i):
         sentence += (", " +output[j])
@@ -53,8 +52,6 @@ def deleteAll():
     deletion = collection.delete_many({})
     casualties = deletion.deleted_count
     return str(casualties) + " casualties have been found! Collection has been fully truncated!"
-
-
 
 
 if __name__ == '__main__':
