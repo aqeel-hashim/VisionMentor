6680f16b307badcbfc368ffa2183680e33568581
Dzolver <kaitoano0@gmail.com>
2017-08-12 15:55:30 +0530

    Record Value Retrieval System using name

--- VisionMentorAPI/VisionMentorAPI.py
+++ VisionMentorAPI/VisionMentorAPI.py
@@ -1,19 +1,26 @@
 from flask import *
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+import pprint
 
 app = Flask(__name__)
 
 client = MongoClient('mongodb://kaitoano0:boss359slasian!@maincluster-shard-00-00-resyk.mongodb.net:27017,maincluster-shard-00-01-resyk.mongodb.net:27017,maincluster-shard-00-02-resyk.mongodb.net:27017/testDB?ssl=true&replicaSet=mainCluster-shard-0&authSource=admin')
-db = client.testDB
-collection = db.testData
+db = client.civilian
+collection = db.city
 
-
-@app.route('/')
-def hello_world():
+#returns count of all the records in the database
+@app.route('/',methods=['GET'])
+def getUserCount():
     s = collection.count()
     return str(s)
 
+#returns the details of the records with the matching username
+@app.route('/user/<string:name>')
+def getUserNames(name):
+   for coll in collection.find({"Name":name},{"_id":0}):
+       print str(coll['Name'])
+   return str(coll['Name'])
 
 if __name__ == '__main__':
     app.run()
