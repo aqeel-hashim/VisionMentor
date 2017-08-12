12e29607b144edea3267172b28e0a7f387687fab
Athif Shaffy <athif.shaffy@gmail.com>
2017-08-12 20:47:17 +0530

    save file as virtual File

--- src/testCompilation.java
+++ src/testCompilation.java
@@ -1,10 +1,16 @@
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
@@ -26,10 +32,14 @@ public class testCompilation implements ApplicationComponent,BulkFileListener {
 
 
         connection.subscribe(VirtualFileManager.VFS_CHANGES, this);
+//        System.out.println(VirtualFileManager.VFS_CHANGES.getDisplayName());
+//        Project project= ProjectManager.getInstance().getOpenProjects()[0];
 
-        //PsiManager.getInstance(project).addPsiTreeChangeListener();
+       // Document document = PsiDocumentManager.getInstance(project).getPsiFile();
+//       PsiManager.getInstance(project).findFile(LocalFileSystem.getInstance().findFileByIoFile());
+//
 
-
+//        PsiManager.getInstance(project).
         DebugActionListner dbg = new DebugActionListner();
 //
 //        DebugActionListner dbl = new DebugActionListner();
@@ -62,8 +72,13 @@ public class testCompilation implements ApplicationComponent,BulkFileListener {
     }
 
     public void after(List<? extends VFileEvent> events) {
-        Messages.showMessageDialog("there are","some changes",Messages.getWarningIcon());
-
+//       if(events.)
+        //System.out.println("event name");
+        //System.out.println(events.getClass().toString());
+//        if(events.getClass().toString().equals("Save All")) {
+//            Messages.showMessageDialog("there are", "some changes", Messages.getWarningIcon());
+//        }
+            events.get(0).getFile();
     }
 
 
