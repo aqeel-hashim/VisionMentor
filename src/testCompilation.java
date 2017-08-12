4c794e2a5908575c2a0162eb851d75fc1ecec559
Athif Shaffy <athif.shaffy@gmail.com>
2017-08-12 13:12:34 +0530

    Init commit

---
+++ src/testCompilation.java
@@ -0,0 +1,36 @@
+import com.intellij.openapi.compiler.CompilationStatusListener;
+import com.intellij.openapi.compiler.CompileContext;
+import com.intellij.openapi.components.ApplicationComponent;
+import org.jetbrains.annotations.NotNull;
+
+public class testCompilation implements ApplicationComponent,CompilationStatusListener {
+    public testCompilation() {
+        initComponent();
+    }
+
+    @Override
+    public void initComponent() {
+        ActionManager am = ActionManager.getInstance();
+        // TODO: insert component initialization logic here
+        CompilationStatusListener cl = new CompilationStatusListener() {
+            @Override
+            public void compilationFinished(boolean aborted, int errors, int warnings, CompileContext compileContext) {
+                System.out.println("compile error");
+                System.out.println(compileContext);
+            }
+        };
+
+
+    }
+
+    @Override
+    public void disposeComponent() {
+        // TODO: insert component disposal logic here
+    }
+
+    @Override
+    @NotNull
+    public String getComponentName() {
+        return "testCompilation";
+    }
+}
